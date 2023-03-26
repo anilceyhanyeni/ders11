70023f09bfc4381aa5f797683e6f86828bad1665
anilceyhanyeni <anilceyhanyeni@gmail.com>
2023-03-26 18:33:31 +0300

    feat: kisi listede filtre metodları denendi

--- src/KisiListe.jsx
+++ src/KisiListe.jsx
@@ -2,10 +2,33 @@ import kisiler from './veri.js'
 import Kisi from './Kisi.jsx'
 
 function KisiListe() {
+    //let ile başlayan dizelerle method kullanıyoruz
+
+    let arananKisi = kisiler.find(eleman => eleman.name.first === "Kristen")
+
+    let kadinVarmi = kisiler.some(eleman => eleman.gender === "female")
+
+    let dogumKosulu = kisiler.every(
+        eleman => {
+            let yil = new Date(eleman.dob.date).getFullYear()
+            return yil > 1950
+        })
+
+    let kadinlar = kisiler.filter(eleman => eleman.gender === "female")
+    let erkekler = kisiler.filter(eleman => eleman.gender === "male")
+
+
+
+
     return (
         <>
             <div className='row'>
-            {kisiler.map((kisi, index) => { return <Kisi veri={kisi} key={index} /> })}
+
+                {dogumKosulu ? <p>Tüm üyeler 1950'den büyük</p> : <p> En az bir kişi 1950'den küçük</p>}
+                {kadinVarmi ? <p>En az 1 kadın üye var</p> : <p>Hiç kadın üye yok.</p>}
+                {kadinlar.map((kisi, index) => { return <Kisi veri={kisi} key={index} /> })}
+                {erkekler.map((kisi, index) => { return <Kisi veri={kisi} key={index} /> })}
+
             </div>
         </>
     )
@@ -13,3 +36,11 @@ function KisiListe() {
 }
 
 export default KisiListe
+
+//            {kisiler.map((kisi, index) => { return <Kisi veri={kisi} key={index} /> })}
+
+
+
+// return div            {<Kisi veri={arananKisi}/>}
+// return div            {kadinVarmi ? <p>En az 1 kadın üye var</p> : <p>Hiç kadın üye yok.</p>}
+// return div            {kadinlar.map((kisi, index) => { return <Kisi veri={kisi} key={index} /> })}
